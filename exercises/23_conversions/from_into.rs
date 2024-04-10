ef95c0595fa7b3f0e9f26ae2955f927ed2e2c96c
iijung <987kmj4@naver.com>
2024-04-10 14:43:46 +0900

    solved: 23_conversions/{from_into, from_str, try_from_into, using_as}.rs
    Progress: [###########################################################>] 95/96 (99.0 %)

--- exercises/23_conversions/from_into.rs
+++ exercises/23_conversions/from_into.rs
@@ -41,10 +41,20 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
-    fn from(s: &str) -> Person {}
+    fn from(s: &str) -> Person {
+        s.split_once(',')
+            .map(|(name, age)| {
+                if name.is_empty() {
+                    Person::default()
+                } else {
+                    age.parse::<usize>()
+                        .map(|age| Person { name: name.to_string(), age})
+                        .unwrap_or_else(|_| Person::default())
+                }
+            })
+            .unwrap_or(Person::default())
+    }
 }
 
 fn main() {
